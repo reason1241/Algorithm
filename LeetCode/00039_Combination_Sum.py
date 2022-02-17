94f72c9748a41156e4ea9f496eb409d72268c111
reason1241 <reason1241@naver.com>
2022-02-17 20:36:28 +0900

    [Add] 리트코드 39번 추가

--- LeetCode/00039_Combination_Sum.py
+++ LeetCode/00039_Combination_Sum.py
@@ -20,3 +20,20 @@ class Solution:
             return result
         candidates.sort()
         return _combinationSum(candidates, target)
+    
+# Time Complexity O(n) 108 ms
+# Space Complexity O(n) 14 MB
+# dfs
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def _combinationSum(candidates: List[int], target: int, path: List[int], result: List[int]):
+            if target < 0:
+                return
+            elif target == 0:
+                result.append(path)
+                return
+            for idx in range(len(candidates)): 
+                _combinationSum(candidates[idx:], target - candidates[idx], path + [candidates[idx]], result)
+        result = []
+        _combinationSum(candidates, target, [], result)
+        return result
