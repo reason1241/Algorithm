5c4e690837bdd5b676ad52e513536e8ff043ab82
reason1241 <reason1241@naver.com>
2022-03-31 23:54:32 +0900

    [Add] 리트코드 41번 추가

--- LeetCode/00041_First_Missing_Positive.py
+++ LeetCode/00041_First_Missing_Positive.py
@@ -11,3 +11,20 @@ class Solution:
             if i not in nums_set:
                 return i
         return -1
+    
+# # Time Complexity O(n) 928 ms
+# # Space Complexity O(1) 59.6 MB
+# class Solution:
+#     def firstMissingPositive(self, nums: List[int]) -> int:        
+#         for i in range(len(nums)):
+#             while 0 < nums[i] <= len(nums):
+#                 if nums[i] == nums[nums[i] - 1] or nums[i] == i + 1: break
+#                 tmp = nums[i] - 1
+#                 nums[i], nums[tmp] = nums[tmp], nums[i]
+                
+#         for i in range(len(nums)):
+#             if nums[i] != i + 1: return i + 1
+        
+#         return len(nums) + 1
+            
+                
