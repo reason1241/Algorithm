668090a3868921be07462e5b14a20a5bc808f579
reason1241 <reason1241@naver.com>
2022-04-28 23:22:38 +0900

    [Add] 리트코드 1631번

---
+++ LeetCode/01631_Path_With_Minimum_Effort.py
@@ -0,0 +1,30 @@
+import heapq
+
+# Time Complexity O(rc log rc)
+# Space Complexity O(rc)
+class Solution:
+    # dijkstra
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        rows, cols = len(heights), len(heights[0])
+        
+        h = [(0, 0, 0)]
+        visited = set()
+        
+        while h:
+            cost, y, x = heapq.heappop(h)
+                        
+            if y == rows - 1 and x == cols - 1:
+                return cost
+            
+            if (y, x) in visited:
+                continue
+            
+            visited.add((y, x))
+            
+            for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                if 0 <= y + dy < rows and 0 <= x + dx < cols and (dy + y, dx + x) not in visited:
+                    next_cost = abs(heights[y][x] - heights[dy + y][dx + x])
+                    heapq.heappush(h, (max(cost, next_cost), y + dy, x + dx))
+                
+        return -1
+        
