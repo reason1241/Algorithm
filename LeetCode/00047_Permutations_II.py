4ccbf4ad87796a711ca9cdc84eeebb7b94cf5ad5
reason1241 <reason1241@naver.com>
2022-05-12 23:23:32 +0900

    [Add] 리트코드 47번

--- LeetCode/00047_Permutations_II.py
+++ LeetCode/00047_Permutations_II.py
@@ -16,4 +16,44 @@ class Solution:
                 
         recursivePermute(nums, [])
         return result
-                    
+
+
+# from collections import Counter
+# # Time Complexity O(n!) 89 ms
+# # Space Complexity O(n) 14.3 MB
+# class Solution:
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+        
+#         def backtracking(cur: List[int], counter: Dict[int, int]):
+#             if len(cur) == len(nums):
+#                 result.append(list(cur))
+#                 return
+            
+#             for i, v in counter.items():
+#                 if v > 0:
+#                     cur.append(i)
+#                     counter[i] -= 1
+#                     backtracking(cur, counter)
+#                     cur.pop()
+#                     counter[i] += 1
+                
+        
+#         backtracking([], Counter(nums))
+#         return result
+
+# # Time Complexity O(n!) 60 ms
+# # Space Complexity O(n) 14 MB
+# class Solution:
+#     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+#         result = [[]]
+        
+#         for num in nums:
+#             new_result = []
+#             for sub in result:
+#                 for i in range(len(sub) + 1):
+#                     new_result.append(sub[:i] + [num] + sub[i:])
+#                     if i < len(sub) and sub[i] == num: 
+#                         break
+#             result = new_result
+#         return result
