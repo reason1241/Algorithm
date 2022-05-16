c45a9d2aff13ed1756c3569183e944fd14d6eb11
reason1241 <reason1241@naver.com>
2022-05-17 00:03:03 +0900

    [Modify] 일일 퀘스트 추가

--- LeetCode/01091_Shortest_Path_in_Binary_Matrix.py
+++ LeetCode/01091_Shortest_Path_in_Binary_Matrix.py
@@ -33,5 +33,28 @@ class Solution:
             q = next_q
             
         return level if grid[n-1][n-1] else -1
+
+# # Time Complexity O(n**2) 1120 ms
+# # Space Complexity O(n) 14.3 MB
+# class Solution:
+#     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+#         n = len(grid)
+#         if grid[0][0] == 1 or grid[n-1][n-1] == 1: return -1
         
+#         q = [(0,0)]
+        
+#         level = 0
+        
+#         while q:
+#             level += 1
+#             for _ in range(len(q)):
+#                 y, x = q.pop(0)
+#                 if y == n-1 and x == n-1: return level
+                
+#                 for dy, dx in [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]:
+#                     if 0 <= y+dy < n and 0 <= x+dx < n and grid[y+dy][x+dx] == 0:
+#                         q.append((y+dy, x+dx))
+#                         grid[y+dy][x+dx] = 1
+                
+#         return -1
         
